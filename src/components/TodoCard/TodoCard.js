37ebb162caa094d528d0de15556164579d82f4e8
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-04-13 08:22:37 -0300

    Marcar o flag de concluido altera o estilo do input

--- src/components/TodoCard/TodoCard.js
+++ src/components/TodoCard/TodoCard.js
@@ -1,31 +1,28 @@
+import { useState } from 'react';
 import './TodoCard.css';
 
-function TodoCard({ id_tarefa, descricao, data_realizado, }) {
+function TodoCard({ id_tarefa, descricao, data_realizado }) {
+
+    const [isChecked, setIsChecked] = useState(data_realizado);
+
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    }
 
     return (
-
         <div className='todoCard' id={id_tarefa}>
-            {
-                data_realizado ?
-                    <input  type='checkbox'
-                            className='todoCard-done'
-                            checked
-                    />
-                    :
-                    <input type='checkbox'
-                        className='todoCard-done'
-                    />
-            }
-
-            <input  type='text'
-                    className='todoCard-input'
-                    value={descricao}
+            <input  type='checkbox'
+                    className='todoCard-done'
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
             />
-            
+            <input  type='text'
+                    className={isChecked ? 'todoCard-input marcado' : 'todoCard-input'}
+                    value={descricao}
+                    readOnly={!isChecked}
+            />
             <div className='todoCardClose'/>
-
         </div>
-
     );
 }
 
